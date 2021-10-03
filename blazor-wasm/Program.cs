840283a0642ad0b46eab57f5650b6081bfdacd32
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2021-10-04 01:47:01 +0700

    Add blazor-wasm implementation

---
+++ blazor-wasm/Program.cs
@@ -0,0 +1,9 @@
+using DecimalClock;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+await builder.Build().RunAsync();
---
+++ blazor-wasm/wwwroot/js/scripts.js
@@ -0,0 +1,20 @@
+﻿// reference to requestAnimationFrame subscription
+let renderRequest;
+
+// reference to blazor page
+let objRef
+
+function startRenderLoop(obj) {
+    objRef = obj;
+    renderRequest = requestAnimationFrame(render);
+}
+
+function stopRenderLoop() {
+    cancelAnimationFrame(renderRequest);
+}
+
+async function render() {
+    // call Render method in blazor page
+    await objRef.invokeMethodAsync("Render")
+    requestAnimationFrame(render);
+}
