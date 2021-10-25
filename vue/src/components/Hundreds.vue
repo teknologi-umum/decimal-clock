ef0b9b995a16eb81e9ad5f6bacf74977dbc32268
Reinaldy Rafli <aldy505@tutanota.com>
2021-10-25 16:52:34 +0700

    feat: implement vue 3 composition api

---
+++ vue/src/components/Hundreds.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { inject } from 'vue';
+
+const hundreds = inject<number[]>('arrOfHundreds');
+</script>
+
+<template>
+  <div
+    v-for="(_, i) in hundreds"
+    v-memo="[i]"
+    :class="i % 10 === 0 ? 'large tick' : 'tick'"
+    :style="`transform: rotate(${i * 3.6}deg);`"
+  />
+</template>
---
+++ vue/src/components/Tens.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { inject } from 'vue';
+
+const tens = inject<number[]>('arrOfTens');
+</script>
+
+<template>
+  <div 
+    v-for="(_, i) in tens"
+    class="digit"
+    :style="`left: ${50-Math.sin((Math.PI * i) / 5) * 40}%; top: ${50+Math.cos((Math.PI * i) / 5) * 40}%;`"
+  >
+    {{ i }}
+  </div>
+</template>
