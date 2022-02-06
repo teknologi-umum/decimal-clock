7173a2327cfe6c7b2d1075cf25f7a5b50f0ae8f0
Krishna Rowter <krishna.rowter@gmail.com>
2022-02-06 09:47:56 +0700

    [Preact] Remove unused code

--- preact/src/views/helpers.ts
+++ preact/src/views/helpers.ts
@@ -13,41 +13,6 @@ export const fractionalArcLengthToXY = (fraction: number, center: number, radius
   };
 };
 
-export const canvasManager = function (ctx: CanvasRenderingContext2D) {
-  const center = ctx.canvas.clientWidth / 2;
-
-  const circleFromCenter = function (radius: number) {
-    ctx.arc(center, center, radius, 0, fullCircle);
-    return this;
-  };
-
-  const loopAround = function (iterations, callback) {
-    for (let i = 0; i < iterations; i++) {
-      ctx.arc(center, center, center, zeroPosition, zeroPosition + (fullCircle * i) / iterations);
-      callback(ctx, i);
-    }
-    return this;
-  };
-
-  const setStyle = function (styleObject: Pick<CanvasRenderingContext2D, "strokeStyle" & "lineWidth" & "fillStyle" & "font">) {
-    ctx.beginPath();
-
-    Object.assign(ctx, styleObject);
-    return this;
-  };
-
-  const stroke = function () {
-    ctx.stroke();
-    return this;
-  };
-  const fill = function () {
-    ctx.fill();
-    return this;
-  };
-
-  return { setStyle, circleFromCenter, loopAround, stroke, fill };
-};
-
 export const getAngleFromTimeValue = (value, type: ClockHandType) => {
   let maxValue
 
