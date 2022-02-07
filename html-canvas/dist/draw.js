5ab9ed126c60410ba3b83d8d771d5c509fb03fe0
Krishna Rowter <42563517+krowter@users.noreply.github.com>
2022-02-07 11:31:46 +0700

    HTML Canvas implementation (#57)
    * [HTML Canvas] Add html canvas implementation
    
    * [HTML Canvas] Remove duplicate code block
    
    * [HTML Canvas] Fix minor issue
    
    * [HTML Canvas] Fix variable name casing

---
+++ html-canvas/dist/draw.js
@@ -0,0 +1,70 @@
+// starter
+window.addEventListener("DOMContentLoaded", () => {
+  requestAnimationFrame(step);
+});
+
+// draw clock with grey border
+canvasManager(ctx)
+  .setStyle({
+    lineWidth: 10,
+    fillStyle: "white",
+    strokeStyle: "grey",
+    textAlign: "center",
+  })
+  .circleFromCenter(radius - 5 /* <- lineWidth / 2 */)
+  .fill()
+  .stroke()
+
+  // draw lines to center
+  .setStyle({ lineWidth: 1 })
+  .loopAround(BASE, (_) => _.lineToCenter())
+  .stroke()
+
+  // cover with circle to create ticks
+  .setStyle({ fillStyle: "white" })
+  .circleFromCenter(radius * (7 / 8))
+  .fill()
+
+  // same process but for larger ticks
+  .setStyle({ lineWidth: 1 })
+  .loopAround(HOURS, (_) => _.lineToCenter())
+  .stroke()
+
+  // cover again
+  .setStyle({ fillStyle: "white" })
+  .circleFromCenter(radius * (9 / 11))
+  .fill()
+
+  // draw numbers
+  .setStyle({ font: `${size / 180}rem Arial`, fillStyle: "black" })
+  .loopAround(HOURS, (_, i) => {
+    const { x, y } = fractionalArcLengthToXY(i / HOURS, radius * (3 / 4));
+    ctx.fillText(i.toString(), x, y);
+  });
+
+// drawing function
+function step() {
+  const { hours, minutes, seconds } = getCentesimalClockValue();
+  canvasManager(ctx)
+    // erase circle portion of previous frame
+    .setStyle({ fillStyle: "white" })
+    .circleFromCenter(radius * (7 / 11))
+    .fill()
+
+    // redraw hands
+    .setStyle({ strokeStyle: "black", lineWidth: 3 })
+    .loopAround([seconds, minutes, hours], (_) => _.lineToCenter(), handLengths)
+    .stroke()
+
+    // redraw center dot
+    .setStyle({ fillStyle: "black" })
+    .circleFromCenter(radius * (1 / 25))
+    .fill()
+
+    // redraw logo
+    .setStyle({ font: "18px Arial", color: "grey" });
+
+  ctx.fillText("TEKNUM", center, center - 100);
+
+  requestAnimationFrame(step);
+}
---
+++ html-canvas/dist/setup.js
@@ -0,0 +1,104 @@
+/* Constants */
+
+const BASE = 100;
+const HOURS = 10;
+
+const SEXAGESIMAL_SECONDS = 86400;
+const CENTESIMAL_SECONDS = HOURS * BASE * BASE;
+
+/* Setup clockface */
+
+const size = 500;
+const radius = size / 2;
+const center = size / 2;
+
+const handLengths = [radius * 0.6, radius * 0.45, radius * 0.35];
+
+const fullCircle = Math.PI * 2;
+const zeroPosition = Math.PI / 2; // so '0' is at bottom of clockface
+
+const clockFace = document.getElementById("clock");
+const ctx = clockFace.getContext("2d");
+
+clockFace.width = size;
+clockFace.height = size;
+
+/* Helper functions */
+
+function fractionalArcLengthToXY(fraction, radius) {
+  return {
+    x: center + radius * Math.cos(zeroPosition + fraction * fullCircle),
+    y: center + 10 + radius * Math.sin(zeroPosition + fraction * fullCircle),
+  };
+}
+
+function canvasManager(ctx) {
+  const circleFromCenter = function (radius) {
+    ctx.arc(center, center, radius, 0, fullCircle);
+    return this;
+  };
+
+  const lineToCenter = function () {
+    ctx.lineTo(center, center);
+    return this;
+  };
+
+  const loopAround = function (iterations, callback, r = radius) {
+    if (Array.isArray(iterations)) {
+      for (let i = 0; i < iterations.length; i++) {
+        ctx.arc(center, center, r[i], iterations[i], iterations[i]);
+        callback(this, i);
+      }
+      return this;
+    }
+
+    for (let i = 0; i < iterations; i++) {
+      ctx.arc( center, center, r, zeroPosition, zeroPosition + (fullCircle * i) / iterations);
+      callback(this, i);
+    }
+    return this;
+  };
+
+  const setStyle = function (styleObject) {
+    ctx.beginPath();
+
+    Object.assign(ctx, styleObject);
+    return this;
+  };
+
+  const stroke = function () {
+    ctx.stroke();
+    return this;
+  };
+  const fill = function () {
+    ctx.fill();
+    return this;
+  };
+
+  return {
+    circleFromCenter,
+    lineToCenter,
+    loopAround,
+    setStyle,
+    stroke,
+    fill,
+  };
+}
+
+function getCentesimalClockValue(date = new Date()) {
+  const midnight = new Date(date).setHours(0, 0, 0, 0);
+
+  const fractionOfToday = (new Date(date).getTime() - midnight) / SEXAGESIMAL_SECONDS / 1000;
+
+  const secondsNow = fractionOfToday * CENTESIMAL_SECONDS;
+
+  const hours = secondsNow / BASE ** 2;
+  const minutes = (secondsNow - Math.floor(hours) * BASE ** 2) / BASE;
+  const seconds = secondsNow - Math.floor(hours) * BASE ** 2 - Math.floor(minutes) * BASE;
+
+  return {
+    hours: (hours / HOURS) * Math.PI * 2 + Math.PI / 2,
+    minutes: (minutes / BASE) * Math.PI * 2 + Math.PI / 2,
+    seconds: (seconds / BASE) * Math.PI * 2 + Math.PI / 2,
+  };
+}
